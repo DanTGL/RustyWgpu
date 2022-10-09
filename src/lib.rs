4c1d5768d95cc4e3b747f93602c70bdfb9a517e4
DanTGL <33489389+DanTGL@users.noreply.github.com>
2022-10-09 20:08:06 +0200

    Added wgpu state

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,105 @@
+use wgpu::Backends;
 use winit::{
 	event::*,
 	event_loop::{ControlFlow, EventLoop},
-	window::WindowBuilder,
+	window::{WindowBuilder, Window},
 };
 
 #[cfg(target_arch="wasm32")]
 use wasm_bindgen::prelude::*;
 
+struct State {
+	surface: wgpu::Surface,
+	device: wgpu::Device,
+	queue: wgpu::Queue,
+	config: wgpu::SurfaceConfiguration,
+	size: winit::dpi::PhysicalSize<u32>,
+}
+
+impl State {
+	// Creating some of the wgpu types requires async code
+	async fn new(window: &Window) -> Self {
+		let size = window.inner_size();
+
+		// The instance is a handle to our GPU
+		// Backends::all => Vulkan + Metal + DX12 + Browser WebGPU
+		let instance = wgpu::Instance::new(wgpu::Backends::all());
+		let surface = unsafe { instance.create_surface(window) };
+		let adapter = instance.request_adapter(
+			&wgpu::RequestAdapterOptions {
+				power_preference: wgpu::PowerPreference::default(),
+				compatible_surface: Some(&surface),
+				force_fallback_adapter: false,
+			},
+		).await.unwrap();
+
+		// The code below might be better than the one above
+		/*
+		let adapter = instance
+    		.enumerate_adapters(wgpu::Backends::all())
+    		.filter(|adapter| {
+        		// Check if this adapter supports our surface
+        		!surface.get_supported_formats(&adapter).is_empty()
+    		})
+    		.next()
+    		.unwrap();*/
+		
+		let (device, queue) = adapter.request_device(
+			&wgpu::DeviceDescriptor {
+				features: wgpu::Features::empty(),
+
+				// WebGL doesn't support all of wgpu's features, so if
+				// we're building for the web we'll have to disable some.
+				limits: if cfg!(target_arch = "wasm32") {
+					wgpu::Limits::downlevel_webgl2_defaults()
+				} else {
+					wgpu::Limits::default()
+				},
+				label: None
+			},
+			None,	// Trace path
+		).await.unwrap();
+
+		let config = wgpu::SurfaceConfiguration {
+			usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+			format: surface.get_supported_formats(&adapter)[0],
+			width: size.width,
+			height: size.height,
+			present_mode: wgpu::PresentMode::Fifo,
+		};
+		surface.configure(&device, &config);
+
+		Self {
+			surface,
+			device,
+			queue,
+			config,
+			size,
+		}
+
+	}
+
+	fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+		todo!()
+	}
+
+	fn input(&mut self, event: &WindowEvent) -> bool {
+		todo!()
+	}
+
+	fn update(&mut self) {
+		todo!()
+	}
+
+	fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+		todo!()
+	}
+}
+
+
+
 #[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
-pub fn run() {
+pub async fn run() {
 	cfg_if::cfg_if! {
 		if #[cfg(target_arch = "wasm32")] {
 			std::panic::set_hook(Box::new(console_error_panic_hook::hook));
@@ -40,6 +131,7 @@ pub fn run() {
     	    .expect("Couldn't append canvas to document body.");
 	}
 
+	let mut state = State::new(&window).await;
 
 	event_loop.run(move |event, _, control_flow| match event {
 		Event::WindowEvent { 

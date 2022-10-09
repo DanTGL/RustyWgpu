19890e9d6dbec2e40a6ff5e0032f114851ed0b99
DanTGL <33489389+DanTGL@users.noreply.github.com>
2022-10-09 22:31:01 +0200

    Implemented shader and rendered triangle

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use wgpu::Backends;
+use wgpu::{Backends, include_wgsl};
 use winit::{
 	event::*,
 	event_loop::{ControlFlow, EventLoop},
@@ -15,6 +15,7 @@ struct State {
 	config: wgpu::SurfaceConfiguration,
 	size: winit::dpi::PhysicalSize<u32>,
 	clear_color: wgpu::Color,
+	render_pipeline: wgpu::RenderPipeline,
 }
 
 impl State {
@@ -70,13 +71,62 @@ impl State {
 		};
 		surface.configure(&device, &config);
 
+		let shader = device.create_shader_module(include_wgsl!("shader.wgsl"));
+
+		let render_pipeline_layout =
+			device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+				label: Some("Render Pipeline Layout"),
+				bind_group_layouts: &[],
+				push_constant_ranges: &[],
+			});
+
+		let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+			label: Some("Render Pipeline"),
+			layout: Some(&render_pipeline_layout),
+			vertex: wgpu::VertexState {
+				module: &shader,
+				entry_point: "vs_main", // Vertex shader entry point function
+				buffers: &[], // Vertices (unused right now)
+			},
+			fragment: Some(wgpu::FragmentState {
+				module: &shader,
+				entry_point: "fs_main", // Fragment shader entry point function
+				targets: &[Some(wgpu::ColorTargetState { // Output information
+					format: config.format,
+					blend: Some(wgpu::BlendState::REPLACE),
+					write_mask: wgpu::ColorWrites::ALL,
+				})],
+			}),
+			primitive: wgpu::PrimitiveState {
+				topology: wgpu::PrimitiveTopology::TriangleList, // Every three vertices correspond to one triangle
+				strip_index_format: None,
+				front_face: wgpu::FrontFace::Ccw,
+				cull_mode: Some(wgpu::Face::Back),
+				// Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+				polygon_mode: wgpu::PolygonMode::Fill,
+				// Requires Features::DEPTH_CLIP_CONTROL
+				unclipped_depth: false,
+				// Requires Features::CONSERVATIVE_RASTERIZATION
+				conservative: false,
+			},
+
+			depth_stencil: None,
+			multisample: wgpu::MultisampleState {
+				count: 1,
+				mask: !0, // Use all samples
+				alpha_to_coverage_enabled: false,
+			},
+			multiview: None,
+		});
+
 		Self {
 			surface,
 			device,
 			queue,
 			config,
 			size,
-			clear_color: wgpu::Color::WHITE
+			clear_color: wgpu::Color::WHITE,
+			render_pipeline,
 		}
 
 	}
@@ -121,7 +171,7 @@ impl State {
 		});
 
 		{
-			let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+			let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
 				label: Some("Render Pass"),
 				color_attachments: &[Some(wgpu::RenderPassColorAttachment {
 					view: &view,
@@ -133,6 +183,9 @@ impl State {
 				})],
 				depth_stencil_attachment: None,
 			});
+
+			render_pass.set_pipeline(&self.render_pipeline);
+			render_pass.draw(0..3, 0..1);
 		}
 
 		// Submit will accept anything that implements IntoIter
